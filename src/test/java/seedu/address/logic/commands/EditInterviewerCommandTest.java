db040a11889981df7c81cc0a014b49113b021d86
Kendrick Ang <phantom2457@hotmail.com>
2019-10-31 23:15:06 +0800

    fix travis CI issues

--- src/test/java/seedu/address/logic/commands/EditInterviewerCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditInterviewerCommandTest.java
@@ -4,5 +4,4 @@ package seedu.address.logic.commands;
  * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests.
  */
 class EditInterviewerCommandTest {
-
 }
